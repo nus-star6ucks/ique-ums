a828eba4fb06f44be1de52c264463ee0f0c93832
xz1996 <xz.cn@outlook.com>
2022-10-04 19:18:29 +0800

    update some config to enable the service

--- src/main/java/com/mtech/ique/ums/model/entity/User.java
+++ src/main/java/com/mtech/ique/ums/model/entity/User.java
@@ -1,14 +1,11 @@
 package com.mtech.ique.ums.model.entity;
 
-import com.mtech.ique.ums.model.enums.UserStatus;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-
 @Setter
 @Getter
-@Entity
+//@Entity
 public class User {
 
     private Long id;
